db92b9eb69a9c4653efbc72d2a7567258a0495dd
bruce <bruce951@sjccs142.saintjoe.edu>
2015-02-10 16:29:44 -0500

    //here is code from page 49//

---
+++ BodyDriver.java
@@ -0,0 +1,14 @@
+public class BodyDriver { 
+	public static void main(String[] args) {
+		Body sun = new Body();
+		sun.idNum = Body.nextID++;
+		sun.name = "Sol";
+		sun.orbits = null; // in solar system, sun is middle 
+	}
+}
+
+
+		
+
+
+
